69384a9938261d682da6886e563bee04aca075bc
Elliot Winkler <elliot.winkler@gmail.com>
2012-08-22 16:51:16 -0600

    Add support for ActiveSupport::Cache::NullStore

---
+++ lib/cache/active_support_cache_null_store.rb
@@ -0,0 +1,11 @@
+require 'cache/active_support_cache_store'
+
+module Cache::ActiveSupportCacheNullStore
+  def self.extended(base)
+    base.extend Cache::ActiveSupportCacheStore
+  end
+
+  def _stats
+    {}
+  end
+end
